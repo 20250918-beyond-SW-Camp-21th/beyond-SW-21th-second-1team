59636fe570c02c534b9abe8604384a8c57adcf49
danielhw99 <danielhw99@gmail.com>
2025-12-04 11:33:37 +0900

    [#5] feat/Reservation file settings

---
+++ Chagok/src/main/java/com/valetparker/chagok/reservation/controller/ReservationController.java
@@ -0,0 +1,10 @@
+package com.valetparker.chagok.reservation.controller;
+
+import com.valetparker.chagok.reservation.domain.Reservation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reservation")
+public class ReservationController {
+}
---
+++ Chagok/src/main/java/com/valetparker/chagok/reservation/service/ReservationService.java
@@ -0,0 +1,9 @@
+package com.valetparker.chagok.reservation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+
+}
