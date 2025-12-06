16f12e223a57460ccf1df3cdaccf5b2e4690bc43
hatahng <hathang16@naver.com>
2025-12-07 05:34:05 +0900

    [#41] feat/user SecurityConfig & HeaderAuthenticationFilter 작성

--- Chagok/src/main/java/com/valetparker/chagok/common/config/SecurityConfig.java
+++ Chagok/src/main/java/com/valetparker/chagok/common/config/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.valetparker.chagok.common.config;
 
+import com.valetparker.chagok.common.jwt.HeaderAuthenticationFilter;
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +20,21 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(HttpMethod.POST, "/regist").permitAll()
+                                .anyRequest().authenticated()
+                ).addFilterBefore(headerAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public HeaderAuthenticationFilter headerAuthenticationFilter() {
+        return new HeaderAuthenticationFilter();
+    }
+
 }
