66e35c633667fb7fef1e371bc6720cdf81c969d8
hatahng <hathang16@naver.com>
2025-12-07 05:35:56 +0900

    [#41] feat/user 회원 가입 코드작성

--- Chagok/src/main/java/com/valetparker/chagok/user/command/controller/UserCommandController.java
+++ Chagok/src/main/java/com/valetparker/chagok/user/command/controller/UserCommandController.java
@@ -1,4 +1,29 @@
 package com.valetparker.chagok.user.command.controller;
 
+import com.valetparker.chagok.common.dto.ApiResponse;
+import com.valetparker.chagok.user.command.domain.User;
+import com.valetparker.chagok.user.command.dto.request.UserCreateRequest;
+import com.valetparker.chagok.user.command.service.UserCommandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
 public class UserCommandController {
+
+    private final UserCommandService userCommandService;
+
+    @PostMapping("/regist")
+    public ResponseEntity<ApiResponse<Void>> registerUser(@RequestBody UserCreateRequest request) {
+        userCommandService.registerUser(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(null));
+    }
+
 }
