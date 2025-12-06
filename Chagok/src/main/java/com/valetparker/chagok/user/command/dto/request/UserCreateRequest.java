3bb3fff1f88e53e4c07a343e8e8f652e4cef68bc
hatahng <hathang16@naver.com>
2025-12-07 05:36:14 +0900

    [#41] feat/user request dto작성

--- Chagok/src/main/java/com/valetparker/chagok/user/command/dto/request/UserCreateRequest.java
+++ Chagok/src/main/java/com/valetparker/chagok/user/command/dto/request/UserCreateRequest.java
@@ -1,5 +1,6 @@
 package com.valetparker.chagok.user.command.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,9 +9,15 @@ import lombok.RequiredArgsConstructor;
 public class UserCreateRequest {
 
     private final String email;
+
     private final String password;
+
+    @JsonProperty("user_name")
     private final String userName;
-    private final String nickName;
+
+    private final String nickname;
+
+    @JsonProperty("car_number")
     private final String carNumber;
 
 }
