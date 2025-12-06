2e138e499fbd997493a7ec00d2e51831377c9458
hatahng <hathang16@naver.com>
2025-12-06 20:18:49 +0900

    [#41] feat/user dto/UserCreateRequest 파일 설정

--- Chagok/src/main/java/com/valetparker/chagok/user/command/dto/request/UserCreateRequest.java
+++ Chagok/src/main/java/com/valetparker/chagok/user/command/dto/request/UserCreateRequest.java
@@ -1,4 +1,16 @@
 package com.valetparker.chagok.user.command.dto.request;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class UserCreateRequest {
+
+    private final String email;
+    private final String password;
+    private final String userName;
+    private final String nickName;
+    private final String carNumber;
+
 }
