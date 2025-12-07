4850e5c2c377276947c34247ad30d842456e7ff1
hatahng <hathang16@naver.com>
2025-12-07 19:10:22 +0900

    [#41] feat/user token 관련 설정

---
+++ Chagok/src/main/java/com/valetparker/chagok/common/jwt/JwtTokenProvider.java
@@ -0,0 +1,101 @@
+package com.valetparker.chagok.common.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("$LH9QZL8upsPBfuDY+Dkb1kT9DZIIUSuA2u4O6Lfi3mkEfeWtETpVTcR/8SMZdJWn/xNTuCQBE6rBvDXgnVmscQ==")
+    private String jwtSecret;
+
+    @Value("180000")
+    private long jwtExpiration;
+
+    @Value("604800000")
+    private long jwtRefreshExpiration;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // 로그인 access token 생성 (닉네임, 차번호, 유저번호)
+    public String createAccessToken(String nickname, String carNumber, Long userNo) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpiration);
+        return Jwts.builder()
+                .subject(nickname)
+                .claim("carNumber",carNumber)
+                .claim("userNo", userNo)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    // refresh token
+    public String createRefreshToken(String nickname, String carNumber, Long userNo) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtRefreshExpiration);
+        return Jwts.builder()
+                .subject(nickname)
+                .claim("carNumber",carNumber)
+                .claim("userNo", userNo)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public long getRefreshTokenExpiration() {
+        return jwtRefreshExpiration;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            throw new BadCredentialsException("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            throw new BadCredentialsException("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            throw new BadCredentialsException("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            throw new BadCredentialsException("JWT Token claims empty", e);
+        }
+
+    }
+
+    public String getEmailFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getSubject();
+    }
+
+    public Date getRefreshTokenExpiryDate(String refreshToken) {
+
+        Claims claims = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(refreshToken)
+                .getPayload();
+
+        return claims.getExpiration(); // exp 값 추출
+    }
+}
