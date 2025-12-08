addc306b412b707f9c29a6835a2392ac5d4a9a85
guntinue <rjssn93@gmail.com>
2025-12-08 12:11:59 +0900

    [#16] feat/payment service update

---
+++ Chagok/src/main/java/com/valetparker/chagok/payment/controller/KakaoPayController.java
@@ -0,0 +1,33 @@
+package com.valetparker.chagok.payment.controller;
+
+import com.valetparker.chagok.payment.client.kakao.KakaoPayProvider;
+import com.valetparker.chagok.payment.client.kakao.dto.request.KakaoPayRequest.OrderRequest;
+import com.valetparker.chagok.payment.client.kakao.dto.response.KakaoPayResponse.ApproveResponse;
+import com.valetparker.chagok.payment.client.kakao.dto.response.KakaoPayResponse.ReadyResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/kakao-pay")
+@Tag(name = "KakaoPay", description = "KakaoPay 기능 관련 API")
+public class KakaoPayController {
+
+    private final KakaoPayProvider kakaoPayProvider;
+
+
+    // 카카오페이 결제를 준비하는 기능을 제공하는 api
+    @PostMapping("/ready")
+    public ReadyResponse ready(@RequestBody OrderRequest request) {
+        return kakaoPayProvider.ready(request);
+    }
+
+    // 카카오페이 결제 성공시 자동으로 호출되는 결제 승인 api
+    @GetMapping("/approve")
+    public ApproveResponse approve(@RequestParam("pg_token") String pgToken) {
+        return kakaoPayProvider.approve(pgToken);
+    }
+
+
+}
