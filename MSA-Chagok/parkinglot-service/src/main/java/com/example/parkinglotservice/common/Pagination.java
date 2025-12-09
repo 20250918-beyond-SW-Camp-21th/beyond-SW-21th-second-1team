4ecb36e22dc564ae8f6c36672c3453a2a6a0bdb9
이상준 <ongsaem@isangjun-ui-MacBookAir.local>
2025-12-09 14:36:58 +0900

    [#81] parkinglot-service setting

---
+++ MSA-Chagok/parkinglot-service/src/main/java/com/example/parkinglotservice/common/Pagination.java
@@ -0,0 +1,12 @@
+package com.example.parkinglotservice.common;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Pagination {
+    private final int currentPage;
+    private final int totalPages;
+    private final long totalItems;
+}
